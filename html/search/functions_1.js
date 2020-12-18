db55097d86bd6beb80bcbf553bb4dbbeb3eb159f
igorsergioJS <64499865+igorsergioJS@users.noreply.github.com>
2020-12-18 01:00:49 -0300

    Add files via upload

---
+++ html/search/functions_1.js
@@ -0,0 +1,4 @@
+var searchData=
+[
+  ['geomview',['geomview',['../class_main_window.html#ad7aae159798833133e2a6092466f1374',1,'MainWindow']]]
+];
---
+++ html/search/functions_6.js
@@ -0,0 +1,11 @@
+var searchData=
+[
+  ['sair',['sair',['../class_main_window.html#a95fd9656f85595b91c4b27b66665e28f',1,'MainWindow']]],
+  ['sculptor',['Sculptor',['../class_sculptor.html#a014e3ef5517bf0e9d9e14486b6ac6433',1,'Sculptor']]],
+  ['selectplane',['selectPlane',['../class_main_window.html#aadb3177083dea6a54fdecf2a3b7338f7',1,'MainWindow']]],
+  ['set_5fb',['set_b',['../class_color_draw.html#ae715a41e049ad0da0e01b452305818aa',1,'ColorDraw']]],
+  ['set_5fg',['set_g',['../class_color_draw.html#a935a117b6ddc47ef2a9655b7502b6d14',1,'ColorDraw']]],
+  ['set_5fr',['set_r',['../class_color_draw.html#a326782c604edcd3cf42bf8e9d5face29',1,'ColorDraw']]],
+  ['setcolor',['setColor',['../class_sculptor.html#a4e53f85ee03b729efafa985f72563c4b',1,'Sculptor']]],
+  ['shape',['shape',['../class_plotter.html#a8ccf4d81ddf7d05b6d9ae98eeab0302e',1,'Plotter']]]
+];
